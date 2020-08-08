e6d4a0ca8132ecbc1cea98caaaecf0374931e1c5
Yu Mitsuhori <ymbullseye@gmail.com>
2020-08-08 19:32:32 +0900

    splash navigation

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,16 +1,16 @@
 import 'package:bot_toast/bot_toast.dart';
 import 'package:calda_app/config/app_themes.dart';
-import 'package:calda_app/model/user.dart';
 import 'package:calda_app/pages/home/home_page.dart';
 import 'package:calda_app/pages/signin/signin_page.dart';
 import 'package:calda_app/pages/signup/signup_page.dart';
 import 'package:calda_app/pages/splash_page.dart';
 import 'package:calda_app/pages/welcome/welcome_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-final userProvider = StateProvider<User>((ref) => User());
+final userProvider = StateProvider<FirebaseUser>((ref) => null);
 
 class App extends HookWidget {
   @override
--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -1,18 +1,16 @@
 import 'package:calda_app/app/app.dart';
-import 'package:calda_app/model/user.dart';
+import 'package:calda_app/pages/home/home_page.dart';
 import 'package:calda_app/pages/welcome/welcome_page.dart';
-import 'package:calda_app/repository/account_repository.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:get_it/get_it.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-final authenticateProvider = FutureProvider<User>(
+final authenticateProvider = FutureProvider<FirebaseUser>(
   (ref) async {
-    final _newUser =
-        await GetIt.instance.get<AccountRepository>().authenticate();
-    ref.read(userProvider).state = _newUser;
-    return _newUser;
+    final user = await FirebaseAuth.instance.currentUser();
+    ref.read(userProvider).state = user;
+    return user;
   },
 );
 
@@ -24,7 +22,7 @@ class SplashPage extends HookWidget {
       if (value == null) {
         return WelcomePage();
       }
-      return Container(); // TODO: HomePage
+      return HomePage();
     }, loading: () {
       return Container();
     }, error: (error, stack) {
